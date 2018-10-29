7b2adf48a07ff91a46bb7941bd023015a229b9f4
windiaini <ainiwindi81@yahoo.com>
2018-10-29 11:26:46 +0700

    upload pertama

---
+++ src/snapapp38/SnapApp38.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package snapapp38;
+
+/**
+ *
+ * @author asus
+ */
+public class SnapApp38 extends SnapFrame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        SnapApp38 a = new SnapApp38();
+        a.setVisible(true);
+    }
+    
+}
